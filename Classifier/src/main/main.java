72289317214727d08be793673dfd2bec4e059ba8
s1677217 <r.h.stribos@student.utwente.nl>
2017-01-26 13:53:06 +0100

    chi-square

--- Classifier/src/main/main.java
+++ Classifier/src/main/main.java
@@ -1,26 +1,29 @@
 package main;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class main {
 
     private static final int smoother = 1;
-    //chi-square test vervanger
-    private static final int top = 1000;
+    private static final int MAX_SIZE = 300;
+    private static final int NUMBER_OF_CLASSES = 2;
 
-    private static int documents = 0;
-    private static int spamDocuments = 0;
-    private static int hamDocuments = 0;
+    private static int[] documents = new int[NUMBER_OF_CLASSES + 1];
 
-    private static Map<String, Integer> spam = new HashMap<>();
-    private static Map<String, Integer> ham = new HashMap<>();
+    private static Map<String, Integer> vocabulary = new HashMap<>();  //de lijst met woorden die worden gebruikt
 
-    private static int spamWords = 0;
-    private static int hamWords = 0;
+    private static Map<String, int[]> wordCount = new HashMap<>(); //telt per woord het aantal per class
+
+    private static Map<String, int[]> wordTable = new HashMap<>();  //de chi-square tabel
+
+    private static int[] words = new int[NUMBER_OF_CLASSES]; //telt het aantal woorden per class
 
     private static int correct = 0;
     private static int incorrect = 0;
@@ -31,12 +34,12 @@ public class main {
             train(i);
         }
 //        train(1);
-        double V = ham.keySet().size();
-        for (String word : spam.keySet()) {
-            V = ham.containsKey(word) ? V : V + 1;
+        double V = wordCount.keySet().size();
+
+        double[] classChance = new double[NUMBER_OF_CLASSES];
+        for (int i = 0 ; i  < NUMBER_OF_CLASSES; i++) {
+            classChance[i] = (double) documents[i + 1] / (double) documents[0] * 100;
         }
-        double hamRatio = (double) hamDocuments /  (double) documents * 100;
-        double spamRatio = (double) spamDocuments / (double) documents * 100;
 
         for (int i = 1; i < 11; i++) {
             //TODO fix path
@@ -46,11 +49,13 @@ public class main {
                 double hamKans = 0;
                 String[] document = normalize(readFile(file.getPath()).split(" "));
                 for (String word : document) {
-                    spamKans += Math.log(spam(word, V));
-                    hamKans += Math.log(ham(word, V));
+                    if (vocabulary.containsKey(word)) {
+                        spamKans += Math.log(classify(word, V, 0));
+                        hamKans += Math.log(classify(word, V, 1));
+                    }
                 }
-                hamKans += Math.log(hamRatio);
-                spamKans += Math.log(spamRatio);
+                hamKans += Math.log(classChance[1]);
+                spamKans += Math.log(classChance[0]);
 //                System.out.println("ham " + hamKans + " spam " + spamKans);
                 if ((hamKans > spamKans && file.getName().contains("-")) || (spamKans > hamKans && file.getName().contains("spmsg"))) {
                     correct++;
@@ -69,16 +74,71 @@ public class main {
         //TODO maakt path compatible
         File dir = new File("C:\\Users\\reinier\\Downloads\\corpus-mails\\corpus-mails\\corpus\\part" + i + "/");
         for (File file : dir.listFiles()) {
-            documents++;
+            documents[0]++;
             if (file.getName().contains("spmsg")) {
-                trainSpam(normalize(readFile(file.getPath()).split(" ")));
-                spamDocuments++;
+                train(normalize(readFile(file.getPath()).split(" ")), 0);
+                documents[1]++;
             } else {
-                trainHam(normalize(readFile(file.getPath()).split(" ")));
-                hamDocuments++;
+                train(normalize(readFile(file.getPath()).split(" ")), 1);
+                documents[2]++;
             }
         }
     }
+
+    private static void chiSquare(String word) {
+        if (!vocabulary.containsKey(word)) {
+            int chiSquare = calculate(word);
+            if (vocabulary.size() > MAX_SIZE) {
+                int min = Collections.min(vocabulary.values());
+                if (chiSquare > min) {
+                    for (String key : vocabulary.keySet()) {
+                        if (vocabulary.get(key) == min) {
+                            vocabulary.remove(key);
+                            break;
+                        }
+                    }
+                    vocabulary.put(word, chiSquare);
+                }
+            } else {
+                vocabulary.put(word, chiSquare);
+            }
+        } else {
+            int chiSquare = calculate(word);
+            vocabulary.replace(word, chiSquare);
+        }
+    }
+
+    private static int calculate(String word) {
+        double[][] table = new double[NUMBER_OF_CLASSES + 1][3];
+
+        int total = 0;
+        for (int i = 0; i < NUMBER_OF_CLASSES; i++) {
+            if (wordTable.containsKey(word)) {
+                total += wordTable.get(word)[i];
+                table[i][0] = wordTable.get(word)[i];
+            } else {
+                table[i][0] = 0;
+            }
+        }
+        table[NUMBER_OF_CLASSES][0] = total;
+        table[NUMBER_OF_CLASSES][2] = total;
+        for (int i = 0; i < NUMBER_OF_CLASSES; i++) {
+            table[i][1] = words[i] - table[i][0];
+            table[i][2] = table[i][0] + table[i][1];
+            table[NUMBER_OF_CLASSES][2] += table[i][0] + table[i][1];
+        }
+
+
+        int result = 0;
+        for (int i = 0; i < NUMBER_OF_CLASSES; i++) {
+            for (int j = 0; j < 2; j++) {
+                double expected = table[i][2] * table[NUMBER_OF_CLASSES][j] / (table[NUMBER_OF_CLASSES][2]);
+                result += Math.pow((table[i][j] - expected), 2) / expected;
+            }
+        }
+        return result;
+    }
+
 
     //moet beter worden
     //haalt hoofdletters weg en shit
@@ -90,40 +150,26 @@ public class main {
         return result;
     }
 
-    private static void trainHam(String[] document) {
+    private static void train(String[] document, int i) {
         for (String word : document) {
-            hamWords++;
-            ham.put(word, ham.containsKey(word) ? ham.get(word) + 1 : 1);
+            words[i] = words[i] + 1;
+            int[] table;
+            if (wordCount.containsKey(word)) {
+                table = wordCount.get(word);
+                table[i] = wordCount.get(word)[i] + 1;
+            } else {
+                table = new int[NUMBER_OF_CLASSES];
+                table[i] = 1;
+            }
+            wordCount.put(word, table);
+            chiSquare(word);
         }
     }
 
-    private static void trainSpam(String[] document) {
-        for (String word : document) {
-            spamWords++;
-            spam.put(word, spam.containsKey(word) ? spam.get(word) + 1 : 1);
-        }
-    }
-
-    private static double ham(String word, double V) {
-        double occurrence = ham.containsKey(word) ? ham.get(word) : 0;
+    private static double classify(String word, double V, int i) {
+        double occurrence = wordCount.containsKey(word) ? wordCount.get(word)[i] : 0;
+        double teller = words[i] + (smoother * V);
         double noemer = occurrence + smoother;
-        //chi-square vervanging
-        if (noemer > top) {
-            return 1;
-        }
-        double teller = hamWords + (smoother * V);
-        double result = noemer / teller;
-        return result;
-    }
-
-    private static double spam(String word, double V) {
-        double occurrence = spam.containsKey(word) ? spam.get(word) : 0;
-        double teller = spamWords + (smoother * V);
-        double noemer = occurrence + smoother;
-        //chi-square vervanging
-        if (noemer > top) {
-            return 1;
-        }
         double result = noemer / teller;
         return result;
     }
